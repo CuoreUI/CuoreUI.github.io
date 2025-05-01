bc822577a0acdf42d81fecbc00e9be4cb12bedde
7ow <115172127+7owh@users.noreply.github.com>
2025-05-01 19:19:23 +0200

    Add files via upload

---
+++ header.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const currentPageClass = "nav-button btn filled";
+    const otherPagesClass = "nav-button btn outlined";
+
+    const headerTemplate = `
+    <header class="top-bar">   
+        <img class="logo" src="assets/cui mono.svg" alt="cui logo"/>
+        <nav class="top-nav"></nav>
+    </header>`;
+
+    document.body.insertAdjacentHTML("afterbegin", headerTemplate);
+
+    const topNav = document.getElementsByClassName("top-nav")[0];
+
+    function AddPage(uri, name)
+    {
+        let a = otherPagesClass;
+        let additionalStyles = "";
+
+        if (document.title === name)
+        {
+            a = currentPageClass;
+        }
+
+        if (uri.startsWith("https:") || uri.startsWith("http:"))
+        {
+            if (name == "Documentation")
+            {
+                additionalStyles = " openlink docs-icon";
+            }
+            else
+            {
+                additionalStyles = " openlink";
+            }
+        }
+
+        const b = `<a href="${uri}"><button class="${a}${additionalStyles}"><p>${name}</p></button></a>`;
+        topNav.insertAdjacentHTML("beforeend", b);
+    }
+
+    AddPage("index.html", "Home");
+    AddPage("https://itzkxhu.gitbook.io/cuoreui", "Documentation");
+    AddPage("https://github.com/7owh/CuoreUI", "Repository");
+    AddPage("contact.html", "Contact");
+});
